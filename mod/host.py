f284d71913949435acc1f2b442cb99cfb50b23a5
jofemodo <fernando@zynthian.org>
2017-12-15 19:42:54 +0100

    [zynthian] Add mod-monitor audio inputs to the web canvas.
    This is needed for zynthian-ui#22: Route audio from standalone engines
    to MOD-UI, allowing to use it as FX processsor

--- mod/host.py
+++ mod/host.py
@@ -951,6 +951,10 @@ class Host(object):
         self.hasSerialMidiIn = has_serial_midi_input_port()
         self.hasSerialMidiOut = has_serial_midi_output_port()
         self.midi_aggregated_mode = has_midi_merger_output_port() or has_midi_broadcaster_input_port()
+        
+        #Add monitor ports for Zynthian routing
+        self.audioportsIn.append("monitor_out_1")
+        self.audioportsIn.append("monitor_out_2")
 
         return True
 
@@ -2745,6 +2749,12 @@ class Host(object):
             if data[2] == "cv_exp_pedal":
                 return "mod-spi2jack:exp_pedal"
 
+            #Zynthian input monitors:
+            if data[2].startswith("monitor_out_"):
+                num = data[2].replace("monitor_out_","",1)
+                return "mod-monitor:out_%s" % num
+            #------------------------
+
             # Default guess
             return "system:%s" % data[2]
 
@@ -2766,6 +2776,8 @@ class Host(object):
                 self.msg_callback("connect %s %s" % (port_from, port_to))
             else:
                 print("ERROR: backend failed to connect ports: '%s' => '%s'" % (port_from, port_to))
+                self.msg_callback("ERROR connecting %s %s" % (self._fix_host_connection_port(port_from),
+                                              self._fix_host_connection_port(port_to)))
 
         self.send_modified("connect %s %s" % (self._fix_host_connection_port(port_from),
                                               self._fix_host_connection_port(port_to)),
